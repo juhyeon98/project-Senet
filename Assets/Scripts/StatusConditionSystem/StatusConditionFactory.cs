407e14799c4f96f45c3dc4fa96cdb5b1d754e3c9
juhyeon <griiim134@gmail.com>
2025-08-14 16:21:32 +0900

    feat: add StatusConditionFactory factory class

---
+++ Assets/Scripts/StatusConditionSystem/StatusConditionFactory.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Juhyeon.StatusConditionSystem
+{
+    public class StatusConditionFactory
+    {
+        public static IStatusCondition MakeStatusCondition(EStatusConditionType type)
+        {
+            switch(type)
+            {
+                case EStatusConditionType.Burn:
+                    break;
+                case EStatusConditionType.Poision:
+                    break;
+                case EStatusConditionType.Curse:
+                    break;
+                case EStatusConditionType.Bleed:
+                    break;
+            }
+            return null;
+        }
+    }
+}
