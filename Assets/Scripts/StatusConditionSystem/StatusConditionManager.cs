fe11a1adbc86a845635e156936a47423a56dc2de
juhyeon <griiim134@gmail.com>
2025-08-14 16:18:51 +0900

    feat: add StatusConditionManager class

---
+++ Assets/Scripts/StatusConditionSystem/StatusConditionManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Juhyeon.StatusConditionSystem
+{
+    public class StatusConditionManager : MonoBehaviour
+    {
+        private Dictionary<EStatusConditionType, IStatusCondition> m_currentStatusConditions;
+
+        public void AddStatusCondition(IStatusCondition statusCondition)
+        {
+            if (!m_currentStatusConditions.ContainsKey(statusCondition.Type))
+            {
+                m_currentStatusConditions.Add(statusCondition.Type, statusCondition);
+            }
+            else
+            {
+                m_currentStatusConditions[statusCondition.Type] = statusCondition;
+            }
+            statusCondition.Begin();
+        }
+
+        public void UpdateStatusCondition()
+        {
+            foreach (var statusCondition in m_currentStatusConditions.Values)
+            {
+                statusCondition.Update();
+
+                if (statusCondition.Count == 0)
+                {
+                    statusCondition.End();
+                    m_currentStatusConditions.Remove(statusCondition.Type);
+                }
+            }
+        }
+    }
+}
