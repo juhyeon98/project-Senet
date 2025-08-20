630be38e183de080012ae04474ba11544fa31af5
juhyeon <griiim134@gmail.com>
2025-08-20 18:39:56 +0900

    feat: add Singleton generic

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -4,53 +4,5 @@ namespace Juhyeon.StageSystem
 {
     public class StageManager : MonoBehaviour
     {
-        private static StageManager instance = null;
-
-        public static StageManager Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = FindFirstObjectByType<StageManager>();
-                    if (instance == null)
-                    {
-                        GameObject managerObject = new GameObject(typeof(StageManager).Name);
-                        instance = managerObject.AddComponent<StageManager>();
-                    }
-                }
-                return instance;
-            }
-        }
-
-        public Stage Stage { get; private set; }
-
-        private void Awake()
-        {
-            if (instance != null && instance != this)
-            {
-                Destroy(gameObject);
-            }
-            else
-            {
-                instance = this;
-                DontDestroyOnLoad(gameObject);
-                LoadStage();
-            }
-        }
-
-        private void LoadStage()
-        {
-            if (Stage == null)
-            {
-                Stage = FindFirstObjectByType<Stage>();
-                if (Stage == null)
-                {
-                    GameObject stageObject = new GameObject(typeof(Stage).Name);
-                    Stage = stageObject.AddComponent<Stage>();
-                }
-            }
-            Stage.Initialize();
-        }
     }
 }
