71e48ffd49aac1d2e7fae4038e2425e93a1dca8c
juhyeon <griiim134@gmail.com>
2025-08-20 19:08:20 +0900

    feat: add Attackable interface

---
+++ Assets/Scripts/Units/IAttackable.cs
@@ -0,0 +1,30 @@
+using Juhyeon.Behaviour;
+using Juhyeon.StatSystem;
+using Juhyeon.StatusConditionSystem;
+using UnityEngine;
+
+namespace Juhyeon.Units
+{
+    public interface IAttackable : IUnit
+    {
+        MovementController MovementController { get; }
+        
+        StatManager StatManager { get; }
+        
+        StatusConditionManager StatusConditionManager { get; }
+        
+        float CurrentAP { get; }
+        
+        float CurrentMOV { get; }
+        
+        float ATK { get; }
+
+        void Attack(IAttackable target);
+
+        void Damage(float atk);
+
+        void Heal(float value);
+
+        void Dead();
+    }
+}
