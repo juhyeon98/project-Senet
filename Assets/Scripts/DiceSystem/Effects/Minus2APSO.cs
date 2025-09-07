1de891530b7e0ace82a68bbb79282098ac70f5bc
juhyeon <griiim134@gmail.com>
2025-09-07 14:52:36 +0900

    feat: add MOV -2 effect

--- Assets/Scripts/DiceSystem/Effects/Minus2APSO.cs
+++ Assets/Scripts/DiceSystem/Effects/Minus2APSO.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Juhyeon.Units;
 
 namespace Juhyeon.DiceSystem
 {
---
+++ Assets/Scripts/DiceSystem/Effects/Minus2MOVSO.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Juhyeon.Units;
+
+namespace Juhyeon.DiceSystem
+{
+	[CreateAssetMenu(fileName = "MOV -2", menuName = "Scriptable Objects/Effect/MOV -2")]
+	public class Minus2MOVSO : DiceEffectSO
+	{
+		#region Descripts
+		public string name = "MOV -2";
+		public string descript = "이동력 -2";
+		#endregion
+
+		public override void ApplyEffect(Player target)
+		{
+			var mov = target.Stat.Base.MOV - 2;
+			target.Stat.UpdateMOV(mov);
+		}
+	}
+}
