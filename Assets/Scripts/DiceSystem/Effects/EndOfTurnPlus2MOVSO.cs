909925ad8c8962858c4b0d83b8e6490281798082
juhyeon <griiim134@gmail.com>
2025-09-07 14:50:22 +0900

    feat: add AP -2 effect

--- Assets/Scripts/DiceSystem/Effects/EndOfTurnPlus2MOVSO.cs
+++ Assets/Scripts/DiceSystem/Effects/EndOfTurnPlus2MOVSO.cs
@@ -4,7 +4,7 @@ using Juhyeon.Units;
 namespace Juhyeon.DiceSystem
 {
 	[CreateAssetMenu(fileName = "MOV +2 when end of turn", menuName = "Scriptable Objects/Effect/MOV +2 when end of turn")]
-	public class EndOfTurnPlus2MOVSO : ScriptableObject
+	public class EndOfTurnPlus2MOVSO : DiceEffectSO
 	{
 		#region Descripts
 		public string name = "MOV +2 when end of turn";
@@ -13,7 +13,7 @@ namespace Juhyeon.DiceSystem
 
 		public override void ApplyEffect(Player target)
 		{
-			if (target.AP == 1)
+			if (target.Stat.AP == 1)
 			{
 				var mov = target.Stat.Base.MOV + 2;
 				target.Stat.UpdateMOV(mov);
---
+++ Assets/Scripts/DiceSystem/Effects/Minus2APSO.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Juhyeon.DiceSystem
+{
+	[CreateAssetMenu(fileName = "AP -2", menuName = "Scriptable Objects/Effect/AP -2")]
+	public class Minus2APSO : DiceEffectSO
+	{
+		#region Descripts
+		public string name = "AP -2";
+		public string descript = "행동력 -2";
+		#endregion
+
+		public override void ApplyEffect(Player target)
+		{
+			var ap = target.Stat.Base.AP - 2;
+			target.Stat.UpdateAP(ap);
+		}
+	}
+}
